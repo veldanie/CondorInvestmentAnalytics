bf7bc7d783e63aef5875339b5c60b35859033840
veldanie <davelasquez@gmail.com>
2018-02-06 08:51:44 -0500

    Update series

--- R/asset_universe.R
+++ R/asset_universe.R
@@ -9,6 +9,8 @@
 
 asset_universe <- function(mean_rets, cov_matrix, lambda) {
 
+
+
   n_assets <- length(mean_rets)
   fitness_fun <- function(x) sum(mean_rets[x == 1])+lambda*sum(cov_matrix[x == 1,x == 1])
 
