498d4ace0e75f24633d79b01bb752a4677294b5d
veldanie <davelasquez@gmail.com>
2019-01-22 15:53:59 -0500

    add default mu and sigma

--- R/rets_complete_index_series.R
+++ R/rets_complete_index_series.R
@@ -5,10 +5,12 @@
 #' @param series Original series,
 #' @param backup_series Series index used to complete original series index.
 #' @param ref_dates reference dates
+#' @param mu Returns mean
+#' @param sigma Return sd
 #' @return index series
 #' @export
 
-rets_complete_index_series <- function(series, backup_series, ref_dates='2000/2017'){
+rets_complete_index_series <- function(series, backup_series, ref_dates='2000/2017', mu = NA, sigma = NA){
 
   all_dates <- index(backup_series)
   ini_date <-index(series)[1]
@@ -16,8 +18,11 @@ rets_complete_index_series <- function(series, backup_series, ref_dates='2000/20
 
   target_dates <- all_dates[all_dates<ini_date]
   rets <- as.vector(coredata(returns(series[ref_dates], type = 'log')))
+  mu_rets <- ifelse(is.na(mu), mean(rets), mu)
+  sigma_rets <- ifelse(is.na(sigma), sd(rets), sigma)
+
   set.seed(42)
-  random_rets <- rnorm(length(target_dates), mean(rets), sd(rets))
+  random_rets <- rnorm(length(target_dates), mu_rets, sigma_rets)
   series_complete <- rbind(series, xts(rev(ini_val * exp(-cumsum(random_rets))), order.by = target_dates))
   return(series_complete)
 }
