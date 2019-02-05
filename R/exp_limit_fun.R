b624fce6a74b287c29802137cb8bf4026c6c4094
veldanie <davelasquez@gmail.com>
2019-02-05 09:57:08 -0500

    add limit exposure for relative constraints

--- R/exp_limit_fun.R
+++ R/exp_limit_fun.R
@@ -4,15 +4,15 @@
 #' Exposure limits functional.
 #' @param f_const Matrix of 0 1 to build weight sums to limit exposure.
 #' @param ind_rel Indicator of relative restriction of an asset wit respect to a group.
-#' @param ind_asset Indicator of relative asset restriction.
+#' @param pos_asset Asset position for relative restriction.
 #' @return Exposure limit function.
 #' @export
 
-exp_limit_fun <- function(f_const, ind_rest, ind_asset) {
+exp_limit_fun <- function(f_const, ind_rest, pos_asset) {
   if(any(ind_rest)){
     function(w){
       w_rest <- as.numeric(f_const %*% w)
-      w_rest[ind_rest] <- w[ind_asset]/w_rest[ind_rest]
+      w_rest[ind_rest] <- w[pos_asset]/w_rest[ind_rest]
       return(w_rest)
     }
   }else{
