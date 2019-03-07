cf9c408cf97c7b0e9c9e4625bdae6d5a1a5faf5b
veldanie <davelasquez@gmail.com>
2019-03-07 10:52:41 -0500

    Check w, mu and Sigma names

--- R/portfolio_return.R
+++ R/portfolio_return.R
@@ -12,8 +12,14 @@ portfolio_return <- function(weights, mean_returns, covar_mat) {
   if(sum(weights)==0){
     port_mean_ret <- port_var <- port_vol <- 0
   }else{
-    port_mean_ret <- as.numeric(t(weights) %*% mean_returns)
-    port_var <- as.numeric(t(weights) %*% covar_mat %*% weights)
+    id <- names(weights)
+    if(is.null(id)){
+      port_mean_ret <- as.numeric(t(weights) %*% mean_returns)
+      port_var <- as.numeric(t(weights) %*% covar_mat %*% weights)
+    }else{
+      port_mean_ret <- as.numeric(t(weights) %*% mean_returns[id])
+      port_var <- as.numeric(t(weights) %*% covar_mat[id, id] %*% weights)
+    }
     port_vol <- sqrt(port_var)
   }
   return(list(port_mean_ret = port_mean_ret, port_var = port_var, port_vol = port_vol))
