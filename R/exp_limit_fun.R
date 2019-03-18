ea7845a1ad3c712137e588db6b732fe3b3266408
veldanie <davelasquez@gmail.com>
2019-03-18 13:13:15 -0500

    exp limit function update: Relative restriction w.r.t. more than 1 asset

--- R/exp_limit_fun.R
+++ R/exp_limit_fun.R
@@ -4,7 +4,7 @@
 #' Exposure limits functional.
 #' @param f_const Matrix of 0 1 to build weight sums to limit exposure.
 #' @param ind_rel Indicator of relative restriction of an asset wit respect to a group.
-#' @param pos_asset Asset position for relative restriction.
+#' @param pos_asset Matrix of assets positions for relative restriction.
 #' @param ind_dur_rest Indicator of duration restriction.
 #' @return Exposure limit function.
 #' @export
@@ -13,14 +13,14 @@ exp_limit_fun <- function(f_const, ind_rel, pos_asset, ind_dur_rest) {
   if(any(ind_rel) && !any(ind_dur_rest)){
     function(w){
       w_rest <- as.numeric(f_const %*% w)
-      w_rest[ind_rel] <- w[pos_asset]/w_rest[ind_rel]
+      w_rest[ind_rel] <- (pos_asset %*% w) /w_rest[ind_rel]
       w_rest[ind_dur_rest] <- w_rest[ind_dur_rest]/sum(w[f_const[ind_dur_rest,]>0])
       return(w_rest)
     }
   }else if(any(ind_rel) && any(ind_dur_rest)){
     function(w){
       w_rest <- as.numeric(f_const %*% w)
-      w_rest[ind_rel] <- w[pos_asset]/w_rest[ind_rel]
+      w_rest[ind_rel] <- (pos_asset %*% w) /w_rest[ind_rel]
       w_rest[ind_dur_rest] <- w_rest[ind_dur_rest]/((f_const[ind_dur_rest,]>0) %*% w)
       return(w_rest)
     }
