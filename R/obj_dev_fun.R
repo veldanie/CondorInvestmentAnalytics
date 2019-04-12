84326df551db073a19e9ec9310599a79890a0aac
veldanie <davelasquez@gmail.com>
2019-04-12 16:27:45 -0500

    update obj dev

--- R/obj_dev_fun.R
+++ R/obj_dev_fun.R
@@ -18,7 +18,11 @@
 obj_dev_fun <- function(w, series, risk_target, ow_assets, uw_assets, per = 12, type = "unif", risk = "vol", quant = 0.95) {
   covar_mat <- cov(series) * per
   vol_ini <- as.numeric(sqrt(t(w) %*% covar_mat %*% w))
+
   w_new<- w
+  neg_rets_ini <- -as.vector(series %*% w)
+  var_ini <- quantile(neg_rets_ini, probs = quant)
+  cvar_ini <- mean(neg_rets_ini[neg_rets_ini > var])
 
   n_ow_assets <- length(ow_assets)
   n_uw_assets <- length(uw_assets)
@@ -27,7 +31,7 @@ obj_dev_fun <- function(w, series, risk_target, ow_assets, uw_assets, per = 12, 
   w_ow_norm <- w_ow/sum(w_ow)
   w_uw_norm <- w_uw/sum(w_uw)
 
-  if(risk == "vol"){
+  if(substr(tolower(risk),1,3) == "vol"){
     if(type=="unif"){
       return(function(x){
         w_new[ow_assets] <- w_ow + x/n_ow_assets
@@ -43,41 +47,40 @@ obj_dev_fun <- function(w, series, risk_target, ow_assets, uw_assets, per = 12, 
       }
       )
     }
-  }else if(risk == "var"){
-    var_ini <- qnorm(quant) * vol_ini
+  }else if(substr(tolower(risk),1,4) == "var" || substr(tolower(risk),1,4) == "ver"){
     if(type=="unif"){
       return(function(x){
         w_new[ow_assets] <- w_ow + x/n_ow_assets
         w_new[uw_assets] <- w_uw - x/n_uw_assets
-        vol_new <- sqrt(t(w_new) %*% covar_mat %*% w_new)
-        return((abs(qnorm(quant) * vol_new  - var_ini) - risk_target)**2)
+        neg_rets_new <- -as.vector(series %*% w_new)
+        return((abs(quantile(neg_rets_new, probs = quant)  - var_ini) - risk_target)**2)
       }
       )
     }else{
       return(function(x){
         w_new[ow_assets] <- w_ow + x*w_ow_norm
         w_new[uw_assets] <- w_uw - x*w_uw_norm
-        vol_new <- sqrt(t(w_new) %*% covar_mat %*% w_new)
-        return((abs(qnorm(quant) * vol_new  - var_ini) - risk_target)**2)
+        neg_rets_new <- -as.vector(series %*% w_new)
+        return((abs(quantile(neg_rets_new, probs = quant)  - var_ini) - risk_target)**2)
       }
       )
     }
-  }else if(risk == "cvar"){
+  }else if(substr(tolower(risk),1,4) == "cvar" || substr(tolower(risk),1,4) == "verc"){
     cvar_ini <- dnorm(qnorm(quant))/(1 - quant) * vol_ini
     if(type=="unif"){
       return(function(x){
         w_new[ow_assets] <- w_ow + x/n_ow_assets
         w_new[uw_assets] <- w_uw - x/n_uw_assets
-        vol_new <- sqrt(t(w_new) %*% covar_mat %*% w_new)
-        return((abs(dnorm(qnorm(quant))/(1 - quant) * vol_new  - cvar_ini) - risk_target)**2)
+        neg_rets_new <- -as.vector(series %*% w_new)
+        return((abs(mean(neg_rets_new[neg_rets_new > quantile(neg_rets_new, probs = quant)])  - cvar_ini) - risk_target)**2)
       }
       )
     }else{
       return(function(x){
         w_new[ow_assets] <- w_ow + x*w_ow_norm
         w_new[uw_assets] <- w_uw - x*w_uw_norm
-        vol_new <- sqrt(t(w_new) %*% covar_mat %*% w_new)
-        return((abs(dnorm(qnorm(quant))/(1 - quant) * vol_new  - cvar_ini) - risk_target)**2)
+        neg_rets_new <- -as.vector(series %*% w_new)
+        return((abs(mean(neg_rets_new[neg_rets_new > quantile(neg_rets_new, probs = quant)])  - cvar_ini) - risk_target)**2)
       }
       )
     }
