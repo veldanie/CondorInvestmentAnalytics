ece455d3ae13f327d18d0112fdb8bf4990c9153f
veldanie <davelasquez@gmail.com>
2019-06-25 09:45:12 -0500

    summary active portfolio

--- R/active_portfolio_summary.R
+++ R/active_portfolio_summary.R
@@ -13,6 +13,11 @@
 active_portfolio_summary <- function(capital, currency, w_port, w_bench, ref_dates, asset_data, series_list, per = "monthly", rebal_per = 1, slippage = 0, commission = 0, port_name = NULL, header_df = c("Retorno Promedio Anual.", "Volatilidad Anual.", "Sharpe", "Retorno Activo", "Tracking Error Anual.", "Razón de Información")) {
 
   freq <- switch(per, 'daily' = 252, 'monthly' = 12, 'quarterly' = 4)
+  if(is.null(w_port) & is.null(w_bench)){ stop("Null portafolios. Check weights!")}
+
+  if(is.null(w_port)){
+    w_port <- w_bench
+  }
 
   asset_names <- unique(names(c(w_port, w_bench)))
   index_curr <- unique(asset_data$Currency[match(asset_names, asset_data$Asset)])
@@ -28,12 +33,12 @@ active_portfolio_summary <- function(capital, currency, w_port, w_bench, ref_dat
 
   te <- active_ret <- ann_te <- info_ratio <- NA
   if(!is.null(w_bench)){
-    bench_back <- portfolio_backtest(w_bench, capital, currency, asset_data, series_back[,names(w_bench)], rebal_per_in_months = rebal_per, slippage = slippage, commission = commission)
-    rets_bench <- periodReturn(benck_back$cash_port, period = per)
+    bench_back <- portfolio_backtest(w_bench, capital, currency, asset_data, series_back[,c(names(w_bench), index_curr)], rebal_per_in_months = rebal_per, slippage = slippage, commission = commission)
+    rets_bench <- periodReturn(bench_back$cash_port, period = per)
     avg_bench <- mean(rets_bench)
 
     te <- sd(rets_port - rets_bench)
-    active_ret <- avg_port - avg_bench
+    active_ret <- round(avg_port - avg_bench, 3)
     ann_te <- round(te*sqrt(freq)*100,3)
     info_ratio <- round(active_ret/te,3)
   }
