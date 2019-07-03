709fa9f21044b7f61838dfbc6c3994b7c63b788f
veldanie <davelasquez@gmail.com>
2019-07-03 11:29:17 -0500

    add mom size argument

--- R/median_of_means.R
+++ R/median_of_means.R
@@ -3,21 +3,28 @@
 #' Median of means.
 #' @param rets Returns series.
 #' @param k Number of groups.
+#' @param pers Number of periods per group.
 #' @return Median of means.
 #' @export
 
-median_of_means <- function(rets, k){
+median_of_means <- function(rets, k = NULL, size = NULL){
   mu <- apply(rets, 2, mean)
-
-  if(length(k)==0){k <- round(as.numeric((tail(index(rets),1) - index(rets)[1])/365)/2)}
   n_rows <- nrow(rets)
-  size <- ceiling(n_rows/k)
+
+  if(!is.null(size)){
+    k <- ceiling(n_rows/size)
+  }else if(is.null(size) && length(k)==0){
+    k <- round(as.numeric((tail(index(rets),1) - index(rets)[1])/365)/2)
+    size <- ceiling(n_rows/k)
+  }else{
+    size <- ceiling(n_rows/k)
+  }
   sample_means <- matrix(0, nrow=k, ncol=ncol(rets))
   for (i in 1:k){
     if(i==k){
-      sample_means[i,] <- apply(rets[(1+size*(i-1)):nrow(rets)],2,mean)
+      sample_means[i,] <- apply(rets[(1+size*(i-1)):nrow(rets),,drop = FALSE],2,mean)
     }else{
-      sample_means[i,] <- apply(rets[(1+size*(i-1)):(size*i)],2,mean)
+      sample_means[i,] <- apply(rets[(1+size*(i-1)):(size*i),,drop = FALSE],2,mean)
     }
   }
   mom <- apply(sample_means,2,median)
