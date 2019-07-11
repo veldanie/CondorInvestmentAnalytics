49d9b8a061b7d43fff8d43c99189b0cf39b099de
veldanie <davelasquez@gmail.com>
2019-07-10 19:24:28 -0500

    remove inf

--- R/returns.R
+++ R/returns.R
@@ -16,12 +16,12 @@ returns <- function(series, dates = NULL, period = 'daily', type = 'arithmetic')
   rets <- periodReturn(series[,1], period = period, type = type, leading  = FALSE)
   if(nvar > 1){
     for (i in 2:nvar){
-      rets_i <- periodReturn(series[,i], period = period, type = type, leading = FALSE)
-      rets_i[is.na(rets_i)] = 0
+      rets_i <- periodReturn(series[,i], period = period, type = type, leading = TRUE)
+      rets_i[is.na(rets_i) | is.infinite(rets_i)] = 0
       rets  <- merge.xts(rets, rets_i)
     }
   }
-  rets <- rets[!apply(is.na(rets), 1, any)]
+  rets <- rets[-1]
   names(rets) <- names(series)
   return(rets)
 }
