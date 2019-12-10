7ca7439e631e55a971569eb53150321f64b792ff
veldanie <davelasquez@gmail.com>
2019-12-09 20:20:38 -0500

    add set.seed

--- R/asset_to_factor_exp.R
+++ R/asset_to_factor_exp.R
@@ -8,15 +8,14 @@
 #' @return Matrix n_assets x n_factors of exposures.
 #' @export
 
-asset_to_factor_exp <- function(assets_rets, factors_rets, lambdas=NULL){
+asset_to_factor_exp <- function(assets_rets, factors_rets, lambdas=NULL, nfolds=5, seed = 123){
   n_assets <- ncol(assets_rets)
   A = matrix(0, n_assets, ncol(factors_rets))
+  set.seed(seed)
   for (i in 1:n_assets){
     asset_rets = assets_rets[,i]
-    dim(factors_rets)
-    dim(asset_rets)
-    model_i = cv.glmnet(x=coredata(factors_rets), y=as.vector(asset_rets), lambda = lambdas)
-    model_i$glmnet.fit
+    foldid = sample(c(1:nfolds), nrow(factors_rets), replace = TRUE)
+    model_i = cv.glmnet(x=coredata(factors_rets), y=as.vector(asset_rets), lambda = lambdas, foldid = foldid)
     fit <- glmnet(x = coredata(factors_rets), y=as.vector(asset_rets), family='gaussian', alpha = 0,
                   lambda = model_i$lambda.min)
     coef <- t(round(as.matrix(fit$beta),5))
