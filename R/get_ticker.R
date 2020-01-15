7c1b804af2da6574f25646688b30a03a9d6e9ff4
veldanie <davelasquez@gmail.com>
2020-01-15 12:02:02 -0500

    update get ticker

--- R/get_ticker.R
+++ R/get_ticker.R
@@ -7,7 +7,17 @@
 #' @return Ticker.
 #' @export
 
-get_ticker <- function(asset, asset_data) {
-  tk <- asset_data$TickerBenchmark[asset_data$Asset==asset]
+get_ticker <- function(assets, asset_data, invest_assets = NULL, fixed_tickers=NULL) {
+  pos_assets <- match(assets, asset_data$Asset)
+  if (!is.null(invest_assets) && invest_assets == "ETF") {
+    tk <- asset_data$TickerETF[pos_assets]
+  }else if (!is.null(invest_assets) && invest_assets == "IA") {
+    tk <- asset_data$TickerInvestAsset[pos_assets]
+    if (!is.null(fixed_tickers)) {
+      tk[match(names(fixed_tickers), assets)] <- fixed_tickers
+    }
+  }else{
+    tk <- asset_data$TickerBenchmark[pos_assets]
+  }
   return(tk)
 }
