7a85a5b134fdc224f5c57aba499fd2941b933f9e
Daniel Velasquez Vergara <daniel.velasquez@sura-am.com>
2020-03-19 13:57:31 -0500

    limit duration of multiasset fund

--- R/exp_limit_fun.R
+++ R/exp_limit_fun.R
@@ -6,10 +6,11 @@
 #' @param ind_rel Indicator of relative restriction of an asset wit respect to a group.
 #' @param pos_asset Matrix of assets positions for relative restriction.
 #' @param ind_dur_rest Indicator of duration restriction.
+#' @param w_adj Adjustment to w. E.g funds.
 #' @return Exposure limit function.
 #' @export
 
-exp_limit_fun <- function(f_const, ind_rel, pos_asset, ind_dur_rest=FALSE) {
+exp_limit_fun <- function(f_const, ind_rel, pos_asset, ind_dur_rest=FALSE, w_adj=rep(1, ncol(f_const))) {
   if(any(ind_rel) && !any(ind_dur_rest)){
     function(w){
       w_rest <- as.numeric(f_const %*% w)
@@ -20,13 +21,13 @@ exp_limit_fun <- function(f_const, ind_rel, pos_asset, ind_dur_rest=FALSE) {
     function(w){
       w_rest <- as.numeric(f_const %*% w)
       w_rest[ind_rel] <- (pos_asset %*% w) /w_rest[ind_rel]
-      w_rest[ind_dur_rest] <- w_rest[ind_dur_rest]/((f_const[ind_dur_rest,]>0) %*% w)
+      w_rest[ind_dur_rest] <- as.numeric(f_const %*% (w*w_adj))[ind_dur_rest]/((f_const[ind_dur_rest,]>0) %*% (w*w_adj))
       return(w_rest)
     }
   }else if(!any(ind_rel) && any(ind_dur_rest)){
     function(w){
-      w_rest <- as.numeric(f_const %*% w)
-      w_rest[ind_dur_rest] <- w_rest[ind_dur_rest]/((f_const[ind_dur_rest,]>0) %*% w)
+      w_rest <- as.numeric(f_const %*% (w*w_adj))
+      w_rest[ind_dur_rest] <- w_rest[ind_dur_rest]/((f_const[ind_dur_rest,]>0) %*% (w*w_adj))
       return(w_rest)
     }
   }else{
