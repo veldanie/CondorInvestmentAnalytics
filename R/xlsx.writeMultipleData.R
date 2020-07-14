af1ae7da374b6380e980d66b919859fa51d77d4c
Juan M. Estrada <juan.estrada@sura-am.com>
2020-07-14 10:36:58 -0500

    Actualización de datos 20200714. Cambio en títulos de gráficos backtesting. Adición de nuevos activos. Cambio en asset funds.

--- R/xlsx.writeMultipleData.R
+++ R/xlsx.writeMultipleData.R
@@ -5,17 +5,16 @@
 #' @param file "filename.xlsx"
 #' @param ... dataframes
 
-xlsx.writeMultipleData <- function (file, ...)
+xlsx.writeMultipleData <- function (file, sep, ...)
 {
-  require(xlsx, quietly = TRUE)
   objects <- list(...)
   fargs <- as.list(match.call(expand.dots = TRUE))
   objnames <- as.character(fargs)[-c(1, 2)]
   nobjects <- length(objects)
   for (i in 1:nobjects) {
     if (i == 1)
-      write.xlsx(objects[[i]], file, sheetName = objnames[i])
+      write.xlsx(objects[[i]], file, sheetName = objnames[i], sep = sep)
     else write.xlsx(objects[[i]], file, sheetName = objnames[i],
-                    append = TRUE)
+                    append = TRUE, sep = sep)
   }
 }
