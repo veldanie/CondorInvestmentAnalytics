f1fc6165b4218ecf84243b349243e1cb89ab21a0
Juan M. Estrada <juan.estrada@sura-am.com>
2020-10-06 11:05:38 -0500

    Actualizacion de datos

--- R/rebuild_series.R
+++ R/rebuild_series.R
@@ -7,10 +7,20 @@
 #' @param ticker ticker
 #' @return rebuilt series
 
-rebuild_series <- function(series_rebuild, real_series, ticker){
-  forecasted_series <- xts(x = series_rebuild[, which(colnames(series_rebuild) == ticker) + 1][!(is.na(series_rebuild[, which(colnames(series_rebuild) == ticker) + 1]))],
-                           order.by = as.Date(series_rebuild[, which(colnames(series_rebuild) == ticker)][!(is.na(as.Date(series_rebuild[, which(colnames(series_rebuild) == ticker)])))],
-                                              format = '%d/%m/%Y'))
+rebuild_series <- function(series_rebuild, real_series, ticker = NULL, search_in_rebuild = FALSE, search_in_real = FALSE){
+  if(search_in_rebuild){
+    forecasted_series <- xts(x = as.numeric(series_rebuild[, which(colnames(series_rebuild) == ticker) + 1])[!(is.na(as.numeric(series_rebuild[, which(colnames(series_rebuild) == ticker) + 1])))],
+                             order.by = as.Date(series_rebuild[, which(colnames(series_rebuild) == ticker)][!(is.na(as.Date(series_rebuild[, which(colnames(series_rebuild) == ticker)])))],
+                                                format = '%d/%m/%Y'))
+  }
+  else{
+    forecasted_series <- series_rebuild
+  }
+  if(search_in_real){
+    real_series <- xts(x = as.numeric(real_series[, which(colnames(real_series) == ticker) + 1])[!(is.na(as.numeric(real_series[, which(colnames(real_series) == ticker) + 1])))],
+                       order.by = as.Date(real_series[, which(colnames(real_series) == ticker)][!(is.na(as.Date(real_series[, which(colnames(real_series) == ticker)])))],
+                                          format = '%d/%m/%Y'))
+  }
   real_series <- returns(real_series, type = 'arithmetic')[toString(paste0(tail(index(forecasted_series), 1), '/')), ]
   real_series <- real_series[!(index(real_series) %in% index(forecasted_series)), ]
   joined_series <- rbind(forecasted_series, real_series)
