ef5b40526b6dcde0d0b0b00f6fb67539fd01f468
Juan M. Estrada <juan.estrada@sura-am.com>
2020-11-04 09:32:26 -0500

    Actualizacion de datos

--- R/portfolio_backtest.R
+++ R/portfolio_backtest.R
@@ -207,11 +207,15 @@ portfolio_backtest <- function(weights, capital, currency, asset_data, series_ba
 
       ##Rebalancing
       if(k < (length(dec_dates)-1)){
+<<<<<<< Updated upstream
         if (k == 1){
           rebal_ind <- (sum(abs(as.vector(weights_xts[dec_dates[k+1],asset_univ])-as.vector(weights[asset_univ]))) > 0.001) | any(abs(as.vector(tail(cash_full_conv,1)/sum(tail(cash_full_conv,1))) - as.vector(weights_xts[dec_dates[k+1],asset_univ])) >= rebal_thres)
         }else{
           rebal_ind <- (sum(abs(as.vector(weights_xts[dec_dates[k+1],asset_univ])-as.vector(weights_xts[dec_dates[k],asset_univ]))) > 0.001) | any(abs(as.vector(tail(cash_full_conv,1)/sum(tail(cash_full_conv,1))) - as.vector(weights_xts[dec_dates[k+1],asset_univ])) >= rebal_thres)
         }
+=======
+        rebal_ind <- any(abs(as.vector(tail(cash_full_conv,1)/sum(tail(cash_full_conv,1))) - as.vector(weights_xts[dec_dates[k+1],asset_univ])) >= rebal_thres)
+>>>>>>> Stashed changes
         if (rebal_ind){
           cash_ini_ref_update <- as.vector(weights_xts[dec_dates[k+1],asset_univ]) * capital_update
         }else{
