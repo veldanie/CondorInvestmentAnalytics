e01bfbbf94d817e472505bc856b6bb19e34382d0
juan.estrada <juan.estrada@sura-am.com>
2021-01-22 09:26:29 -0500

    Cambio en función quarterly_to_semianually

--- R/quarterly_to_semiannualy.R
+++ R/quarterly_to_semiannualy.R
@@ -14,6 +14,14 @@ quarterly_to_semiannualy <- function(rets, type='arithmetic'){
   }
   nrets <- nrow(rets)
   pos_semi <- unique(c(seq(2,nrets,by = 2),nrets))
-  rets_semi <- xts(rets_cumprod[pos_semi]/c(1,rets_cumprod[pos_semi][1:(length(pos_semi)-1)])-1, order.by = index(rets)[pos_semi])
+  nvar <- ncol(rets)
+  rets_semi <- xts(rets_cumprod[pos_semi, 1]/c(1,rets_cumprod[pos_semi, 1][1:(length(pos_semi)-1)])-1, order.by = index(rets)[pos_semi])
+  if(nvar > 1){
+    for(i in 2:nvar){
+      rets_i <- xts(rets_cumprod[pos_semi, i]/c(1,rets_cumprod[pos_semi, i][1:(length(pos_semi)-1)])-1, order.by = index(rets)[pos_semi])
+      rets_semi <- merge.xts(rets_semi, rets_i)
+    }
+    colnames(rets_semi) <- colnames(rets)
+  }
   return(rets_semi)
 }
