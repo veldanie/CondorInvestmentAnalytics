903ccf2d1d524167a8c49fb4ad69a5f19ef53c86
juan.estrada <juan.estrada@sura-am.com>
2021-01-27 16:25:17 -0500

    Actualización de datos y cambio en implied_return

--- R/implied_return.R
+++ R/implied_return.R
@@ -14,7 +14,7 @@ implied_return <- function(lambda, Sigma, w, per = 12, rfr = 0) {
   w_sort <- w[match(names(w), colnames(Sigma))]
   implied_ret <- as.vector((lambda * Sigma %*% w_sort))
   names(implied_ret) <- names(w_sort)
-  implied_ret_annual <- rfr + implied_ret * per
+  implied_ret_annual <- implied_ret * per
 
 
   return(list(implied_ret = implied_ret, implied_ret_ann = implied_ret_annual))
