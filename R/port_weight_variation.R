1bad41564cc18990293e8dc6bc8a6ec12c0dcca7
juan.estrada <juan.estrada@sura-am.com>
2021-03-11 11:51:35 -0500

    nuevas funciones complete_all_dates y port_weight_variation

---
+++ R/port_weight_variation.R
@@ -0,0 +1,13 @@
+
+port_weight_variation <- function(port_weights, asset_series, ini_date, end_date){
+  output_df <- xts(x = matrix(nrow = length(index(asset_series[paste0(c(ini_date, end_date), collapse = '/')])),
+                  ncol = ncol(asset_series)), order.by = index(asset_series[paste0(c(ini_date, end_date), collapse = '/')]))
+  colnames(output_df) <- colnames(asset_series)
+  output_df[1, ] <- 1000 * port_weights[colnames(asset_series)]
+  asset_rets <- returns(asset_series)[paste0(c(ini_date, end_date), collapse = '/')]
+  for(i in 1:length(colnames(output_df))){
+    output_df[2:nrow(output_df), i] <- as.vector(output_df[1, i]) * cumprod(1 + as.vector(asset_rets[2:nrow(asset_rets), i]))
+  }
+  output_df / rowSums(output_df)
+}
+
