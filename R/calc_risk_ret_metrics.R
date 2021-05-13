4cfcba4f71b1762901ec8867900255d736ffc474
juan.estrada <juan.estrada@sura-am.com>
2021-05-13 11:13:48 -0500

    cambio en calc_risk_ret_metrics

--- R/calc_risk_ret_metrics.R
+++ R/calc_risk_ret_metrics.R
@@ -13,12 +13,9 @@ calc_risk_ret_metrics <- function(bench_series, port_series, per = 'monthly', ye
     rets_port1_temp <- rets_port1[paste0(slice_since, '/')]
     rets_port2_temp <- rets_port2[paste0(slice_since, '/')]
 
-    v_cuota <- matrix(0, nrow(bench_series[paste0(slice_since, '/')]), 2)
-    colnames(v_cuota) <- c("Bench series", "Port series")
-    v_cuota[, 'Bench series'] <- 1000 * cumprod(1 + c(0, returns(bench_series)[paste0(slice_since, '/')][2:length(returns(bench_series)[paste0(slice_since, '/')])]))
-    v_cuota[, 'Port series'] <- 1000 * cumprod(1 + c(0, returns(port_series)[paste0(slice_since, '/')][2:length(returns(port_series)[paste0(slice_since, '/')])]))
-    v_cuota <- data.frame(v_cuota)
-    rownames(v_cuota) <- index(returns(port_series)[paste0(slice_since, '/')])
+    v_cuota_bseries <- xts(x = 1000 * cumprod(1 + c(0, returns(bench_series)[paste0(slice_since, '/')][2:length(returns(bench_series)[paste0(slice_since, '/')])])), order.by = index(returns(bench_series)[paste0(slice_since, '/')]))
+    v_cuota_pseries <- xts(x = 1000 * cumprod(1 + c(0, returns(port_series)[paste0(slice_since, '/')][2:length(returns(port_series)[paste0(slice_since, '/')])])), order.by = returns(port_series)[paste0(slice_since, '/')])
+    v_cuota <- merge.xts(v_cuota_bseries, v_cuota_pseries)
     output[[i]]['Valor cuota'] <- list(v_cuota)
 
     avg_port1 <- mean(rets_port1_temp)
