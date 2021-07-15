22dfb92560b71c86e313939e906f57938105a749
Juan Martin Estrada Lopez <juan.estrada@sura-am.com>
2021-07-15 08:51:07 -0500

    cambio en funcion calc_risk_ret_metrics

--- R/calc_risk_ret_metrics.R
+++ R/calc_risk_ret_metrics.R
@@ -15,7 +15,7 @@ calc_risk_ret_metrics <- function(bench_series, port_series, per = 'monthly', ye
     rets_port1_temp <- rets_port1[paste0(slice_since, '/')]
     rets_port2_temp <- rets_port2[paste0(slice_since, '/')]
 
-    if(index(bench_series) == slice_since){
+    if(index(bench_series)[1] == slice_since){
       v_cuota_bseries <- xts(x = 1000 * cumprod(1 + c(0, returns(bench_series)[paste0(slice_since, '/')])), order.by = index(bench_series[paste0(slice_since, '/')]))
       v_cuota_pseries <- xts(x = 1000 * cumprod(1 + c(0, returns(port_series)[paste0(slice_since, '/')])), order.by = index(port_series[paste0(slice_since, '/')]))
     }else{
