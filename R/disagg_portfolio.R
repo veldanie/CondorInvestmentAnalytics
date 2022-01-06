4543bed54933f3de12ae4efe5d205e968176726d
Juan Martin Estrada Lopez <juan.estrada@sura-am.com>
2022-01-06 11:02:14 -0500

    funcion disagg_portfolio

--- R/disagg_portfolio.R
+++ R/disagg_portfolio.R
@@ -2,26 +2,32 @@
 #' Dissagregate portfolio
 #'
 #' Dissagregates portfolio with funds into assets.
-#' @param w weights vector.
+#' @param w_i weights vector.
 #' @param benchmarks DF benchmarks.
+#' @param conn database connection
 #' @return Vector of assets weights.
 #' @export
 
-disagg_portfolio <- function(w, benchmarks) {
-  fund_ind <- names(w) %in% benchmarks$Benchmark
-  if(any(fund_ind)){
-    funds <- names(w)[fund_ind]
-    w_funds <- c()
-    for(fi in funds){
-      w_fund_df <- benchmarks %>% filter(Benchmark==fi) %>% dplyr::select(Asset, Weight)
-      w_fund <- w[fi] * as.numeric(w_fund_df$Weight)
-      names(w_fund) <- w_fund_df$Asset
+disagg_portfolio <- function(w_i, benchmarks, conn){
+  ind_fund_db <- names(w_i) %in% as.data.frame(conn %>% tbl("Weights"))$PortId
+  ind_fund <- names(w_i) %in% benchmarks$Benchmark
+  w_funds <- c()
+  if(any(ind_fund_db)){
+    for(fi in names(w_i[ind_fund_db])){
+      w_df_db <- as.data.frame(conn %>% tbl("Weights") %>% filter(PortId == fi) %>% dplyr::select(Asset, Weight, DatePort))
+      w_df_db <- w_df_db[w_df_db$DatePort == max(w_df_db$DatePort), c("Asset", "Weight")]
+      w_fund_db <- w_i[fi] * as.numeric(w_df_db$Weight)
+      names(w_fund_db) <- w_df_db$Asset
+      w_funds <- c(w_funds, w_fund_db)
+    }
+  }
+  if(any(ind_fund)){
+    for (fi in names(w_i[ind_fund])){
+      w_df <- benchmarks %>% filter(Benchmark==fi) %>% dplyr::select(Asset, Weight)
+      w_fund <- w_i[fi] * as.numeric(w_df$Weight)
+      names(w_fund) <- w_df$Asset
       w_funds <- c(w_funds, w_fund)
     }
-    w_assets <- tapply(c(w[!fund_ind], w_funds), names(c(w[!fund_ind], w_funds)), sum)
-
-  }else{
-    w_assets <- w
   }
-  return(w_assets)
+  w_i <- tapply(c(w_i[!ind_fund], w_funds), names(c(w_i[!ind_fund], w_funds)), sum)
 }
