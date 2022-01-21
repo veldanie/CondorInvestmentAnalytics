8406a3ee4213dc9eaf75688762eb53ec5a66c074
Daniel Velásquez <daniel.velasquez@sura-im.com>
2022-01-21 11:11:03 -0500

    update max_drawdown function to improve precision

--- R/max_drawdown.R
+++ R/max_drawdown.R
@@ -1,23 +1,17 @@
 #' Max Drawdown Return
 #'
-#' Estimates de covariance using weighted averages of products of past returns.
-#' @param per Dates period.
-#' @param w Portfolio weights.
-#' @param series data frame series.
-#' @param type Type of returns: arithmetic (discrete) or log (continuous)
-#' @return Max Drawdown for particular period.
+#' Max Drawdown.
+#' @param series xts series.
 #' @export
 
-max_drawdown <- function(per, w, series, type){
-  series_per <- series[per]
-  w_mat <- rep(1, nrow(series_per)) %*% t(w)
-
-  if(type == 'log')
-    series_ret <- w_mat * log(series_per/(rep(1, nrow(series_per)) %*% head(series[per], 1)))
-  else{
-    series_ret <- w_mat * (series_per/(rep(1, nrow(series_per)) %*% head(series[per], 1)) - 1)
-  }
-  port_ret <- as.numeric(apply(series_ret,1,sum))
-  dd_obs <- max(port_ret) - tail(port_ret,1)
-  return(dd_obs)
+max_drawdown <- function(series){
+  series_cummax <- cummax(series)
+  dd_rets <- series/series_cummax - 1
+  dd <- min(dd_rets)
+  pos_end <- which.min(dd_rets)
+  pos_ini <- which.max(series_cummax[1:pos_end])
+  date_ini=index(series)[pos_ini]
+  date_last=index(series)[pos_end]
+  dd_t <- round(as.numeric(date_last - date_ini)/30, 2)
+  return(list(dd=abs(dd), date_ini=date_ini, date_last=date_last, dd_t=dd_t))
 }
