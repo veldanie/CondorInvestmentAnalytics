e1d02b1cc15ad58a51e9021a0823f16352514ea2
Daniel Velásquez <daniel.velasquez@sura-im.com>
2022-01-26 15:20:52 -0500

    add update_mu arg

--- R/posterior_params.R
+++ R/posterior_params.R
@@ -11,21 +11,28 @@
 #' @return Returns.
 #' @export
 
-posterior_params <- function(mu, q, tau, Sigma, P, Omega = NULL, conf = 0.5){
-
-  if (sum(P!=0) == 0 | is.null(P)){bl_ret = mu; post_var = Sigma
+posterior_params <- function(mu, q, tau, Sigma, P, Omega = NULL, conf = 0.5, update_mu=FALSE){
+  
+  if (update_mu && !is.null(P) && all(rowSums(P)==1)){ # All are absolutr views
+    bl_ret <- mu
+    bl_ret[apply(P, 1, which.max)] <- q
+    post_var <- Sigma      
+  }else{
+    if (sum(P!=0) == 0 | is.null(P)){bl_ret = mu; post_var = Sigma
     }else{
-  if(is.null(Omega)){
-    conf <- (1-conf)/conf
-    Omega <- diag(tau * diag(P%*% Sigma %*% t(P)), nrow = nrow(P), ncol = nrow(P))*conf
+      if(is.null(Omega)){
+        conf <- (1-conf)/conf
+        Omega <- diag(tau * diag(P%*% Sigma %*% t(P)), nrow = nrow(P), ncol = nrow(P))*conf
+      }
+      omega_inv <- solve(Omega)
+      tSigma_inv <- solve(tau * Sigma)
+      post_ret <- solve(tSigma_inv + t(P)%*%omega_inv%*%P)%*%(tSigma_inv %*% mu+t(P) %*% omega_inv %*% q)
+      bl_ret <- as.vector(post_ret)
+      names(bl_ret) <- rownames(post_ret)
+      
+      post_var <- (1+tau)*Sigma - tau^2 * Sigma %*% t(P) %*% solve(P %*% (tau*Sigma) %*% t(P) + Omega) %*% P %*% Sigma
+    }
   }
-  omega_inv <- solve(Omega)
-  tSigma_inv <- solve(tau * Sigma)
-  post_ret <- solve(tSigma_inv + t(P)%*%omega_inv%*%P)%*%(tSigma_inv %*% mu+t(P) %*% omega_inv %*% q)
-  bl_ret <- as.vector(post_ret)
-  names(bl_ret) <- rownames(post_ret)
-
-  post_var <- (1+tau)*Sigma - tau^2 * Sigma %*% t(P) %*% solve(P %*% (tau*Sigma) %*% t(P) + Omega) %*% P %*% Sigma
-  }
+  
   return(list(post_ret = bl_ret, post_var = post_var))
 }
