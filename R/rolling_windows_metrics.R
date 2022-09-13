6ad8a7651fc39baa2ad25df2d6ff51acd19a61bb
LauraNataliaLopez <104458495+LauraNataliaLopez@users.noreply.github.com>
2022-09-13 08:56:20 -0500

    New Function, rolling_windows_metrics.R, calculates rowling windows applying a given function

---
+++ R/rolling_windows_metrics.R
@@ -0,0 +1,30 @@
+#' rolling_windows_metrics 
+#'
+#' Create a rolling window from the data, applying function. Get distribution by quantile.
+#' @param data_series xts portfolios series.
+#' @param returns boolean the data are returns
+#' @param period returns period, as string ('daily','monthly','quarterly','semiannualy')
+#' @param port_names Header names.
+#' @param funct Function to apply 
+#' @param windows_width integer specifying the window width (in numbers of observations) which is aligned to the original sample according to the align argument
+#' @param align specifyies whether the index of the result should be left- or right-aligned or centered [c("center", "left", "right")] compared to the rolling window of observations.
+#' @param quantile_dist Qunatile for the distribution
+#' @param factor_val Constant thar multiplies output metrics
+#' @return Absolute and relative metrics dataframes.
+#' @export
+
+rolling_windows_metrics <- function(data_series, returns = FALSE, period = "monthly", port_names = NULL, func = mean, windows_width = 12, align = c('right'), quantile_dist = 5, factor_val = 100){
+  if (!returns){
+    data_series <- returns(data_series, period=period)
+  }
+  ports_rolling_fun <- na.omit(rollapply(data_series, windows_width, func,  align = align))
+  if(!is.null(port_names)){
+    colnames(ports_rolling_fun) <- port_names
+  }
+  var_rets_dw <- apply(ports_rolling_fun, 2, function (x) round(quantile(x, probs = quantile_dist/100), 3))
+  var_rets_up <- apply(ports_rolling_fun, 2, function (x) round(quantile(x, probs = 1 - quantile_dist/100), 3))
+  var_date <- format(index(ports_rolling_fun[apply(abs(ports_rolling_fun-var_rets_dw), 2, which.min)]), "%b%Y")
+  mean_ret <- apply(ports_rolling_fun, 2, function(x) round(mean(x),3))
+  xticks <- apply(ports_rolling_fun, 2, function(x) pretty(x)*factor_val)
+  return(list(rolling_data = ports_rolling_fun*factor_val, var_rets_dw = var_rets_dw*factor_val, var_rets_up = var_rets_up*factor_val, var_date = var_date, mean_ret = mean_ret*factor_val, xticks = xticks))
+}
