16c59826bc918e5baec6499a27579f8a9a74051e
Daniel Velásquez <daniel.velasquez@sura-im.com>
2022-12-06 21:56:11 -0500

    fix typo active_portfolio_factor_summary

--- R/active_portfolio_factor_summary.R
+++ R/active_portfolio_factor_summary.R
@@ -125,7 +125,7 @@ active_portfolio_factor_summary <- function(capital, currency, w_port, w_bench, 
         rebal_dates <- index(weights_tac)
       }
     }
-    bench_back <- portfolio_backtest(w_bench, capital, currency, asset_data, series_bench[,c(names(w_bench), bench_curr)], rebal_per_in_months = rebal_per, weights_xts = NULL,
+    bench_back <- portfolio_backtest(w_bench, capital, currency, asset_data, series_bench[,c(names(w_bench), bench_curr)], rebal_per_in_months = rebal_per, weights_xts = weights_bench,
                                      rebal_dates = rebal_dates, slippage = slippage, commission = commission)
     total_bench <-as.numeric(tail(bench_back$ret_port,1))
     
