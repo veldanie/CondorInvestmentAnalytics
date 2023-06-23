4f0961457b32a75db903978efa97bf53f4b0b470
Daniel Velásquez <daniel.velasquez@sura-im.com>
2023-06-23 14:45:45 -0500

    update port_duration

--- R/port_duration.R
+++ R/port_duration.R
@@ -7,7 +7,7 @@
 #' @return List of portfolios duration, assets normalized duration, assets normalized weight, and assets original duration.
 #' @export
 
-port_duration <- function (asset_data, assets_w)
+port_duration <- function (asset_data, assets_w, dur_field='Duration')
 {
   
   if(length(assets_w)==0){
@@ -15,15 +15,9 @@ port_duration <- function (asset_data, assets_w)
   }
   
   assets_names <- row.names(assets_w)
-  ports_dur <-  as.double(asset_data$DurMod[match(assets_names,asset_data$Asset)])
-  pos_na <- is.na(ports_dur)
-  ports_dur[pos_na] <- 0
-  assets_w[pos_na,] <- 0
-  sum_dur <- apply(assets_w,2,sum)
-  assets_w_norm <- assets_w/sum_dur
-  ports_dur_norm <- ports_dur*assets_w_norm
-  ports_final_dur <- apply(ports_dur_norm,2,sum)
-  names(ports_dur) <- row.names(assets_w)
-  
-  return(list(ports_final_dur = ports_final_dur, ports_dur_norm = ports_dur_norm, assets_w_norm = assets_w_norm, ports_dur = ports_dur))
+  assets_dur <-  as.double(asset_data[[dur_field]][match(assets_names,asset_data$Asset)])
+  pos_na <- is.na(assets_dur)
+  assets_dur[pos_na] <- 0
+  ports_dur <- (t(assets_dur) %*% as.matrix(assets_w))[1,]
+  return (ports_dur) 
 }
